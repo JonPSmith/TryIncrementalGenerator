93e21994bcf55656fd90533533f3625306eb9bf6
Jon P Smith <jon.smith@selectiveanalytics.com>
2023-03-07 13:57:04 +0000

    Add project files.

---
+++ MySourceGenerator/FirstSourceGenerator.cs
@@ -0,0 +1,15 @@
+﻿// Copyright (c) 2023 Jon P Smith, GitHub: JonPSmith, web: http://www.thereformedprogrammer.net/
+// Licensed under MIT license. See License.txt in the project root for license information.
+
+using Microsoft.CodeAnalysis;
+
+namespace MySourceGenerator;
+
+[Generator]
+public class FirstSourceGenerator : IIncrementalGenerator
+{
+    public void Initialize(IncrementalGeneratorInitializationContext context)
+    {
+        throw new Exception("Test exception!"); // delete me after test
+    }
+}
